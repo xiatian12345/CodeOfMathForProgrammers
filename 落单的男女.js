16ddfde5818a0208b11e4ab7217563604fdc7081
kangchao <kangchao@100tal.com>
2020-03-11 01:11:37 +0800

    Q9:落单的男女

---
+++ 落单的男女.js
@@ -0,0 +1,23 @@
+let getResult = function (man, woman) {
+	let arr = [];
+	for (let i = 0; i < woman + 1; i++) {
+		let temp = [];
+		for (let j = 0; j < man + 1; j++) {
+			temp.push(0);
+		}
+		arr.push(temp);
+	}
+	arr[0][0] = 1;
+
+	for (let i = 0; i < woman + 1; i++) {
+		for (let j = 0; j < man + 1; j++) {
+			if ((i != j) && (j - i) != (man - woman)) {
+				if (i > 0) arr[i][j] += arr[i - 1][j];
+				if (j > 0) arr[i][j] += arr[i][j - 1];
+			}
+		}
+	}
+	console.log(arr);
+	return arr[woman][man - 1] + arr[woman - 1][man];
+};
+console.log(getResult(20, 10));
